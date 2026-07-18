3d4d80100757aa34c6b4ba406af074f2c6912fd3
Katerrio Kennedy <kkennedy@basecampcodingacademy.org>
2026-07-18 16:10:55 -0500

    FInished

--- battle.py
+++ battle.py
@@ -1,70 +1,51 @@
-"""
-RPG Battle System - Starter Code
-Fill in the TODOs to complete the classes benchmark.
-"""
-
-
 class Character:
     def __init__(self, name: str, health: int, attack_power: int):
-        # TODO: store name, health (current), max_health, attack_power
         self.name = name
         self.health = health
         self.maxhealth = self.health        
         self.attack_power = attack_power
-        # TODO: set self.weapon = None
         self.weapon = None
         
 
     def is_alive(self):
-        # TODO: return True if health > 0
         if self.health > 0:
             return True
 
     def take_damage(self, amount):
-        # TODO: subtract amount from health, don't let it go below 0
         self.health -= amount
         if self.health < 0:
             self.health = 0
 
     def attack(self, other):
-        # TODO: deal self.attack_power damage to other, print a message
         other.take_damage(self.attack_power)
         print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
         
 
     def heal(self, amount):
-        # TODO: increase health by amount, don't exceed max_health
         self.health += amount
         if self.health > self.maxhealth:
             self.health = self.maxhealth
         
 
     def equip(self, weapon):
-        # TODO: store the weapon, increase attack_power by weapon.bonus_damage,
-        # print a message
         self.weapon = weapon
         self.attack_power += weapon.bonus_damage
-        print(f"{self.name} equips {self.weapon_name}. Attack power is now {self.attack_power}!")
+        print(f"{self.name} equips {weapon.weapon_name}. Attack power is now {self.attack_power}!")
         
 
 
 class Weapon:
     def __init__(self, name: str, bonus_damage: int):
-        # TODO: store name and bonus_damage
         self.weapon_name = name
         self.bonus_damage = bonus_damage
 
 
 class Battle:
     def __init__(self, fighter_one, fighter_two):
-        # TODO: store both fighters
         self.fighter_one = fighter_one
         self.fighter_two = fighter_two
 
     def run(self):
-        # TODO: alternate turns (fighter_one attacks first),
-        # stop when one fighter's is_alive() is False,
-        # print the winner
         while self.fighter_one.is_alive() and self.fighter_two.is_alive():
             self.fighter_one.attack(self.fighter_two)
             if self.fighter_two.is_alive():
@@ -80,14 +61,10 @@ def main():
     pencil = Weapon("Pencil", 24)
     fighter_one.equip(pencil)
     fighter_two.equip(folding_blade)
+    battle = Battle(fighter_one=fighter_one, fighter_two=fighter_two)
+    battle.run()
           
 
 
 if __name__ == "__main__":
-    # TODO:
     main()
-    # 1. Create two Character instances with different stats
-    # 2. Create at least one Weapon and equip it to one character
-    # 3. Create a Battle with the two characters
-    # 4. Call .run()
-    
